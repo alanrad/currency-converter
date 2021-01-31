5d90810435571147246df508415b844ee6509cf4
Alan Rad <Alan.Rad@bksv.com>
2021-02-01 09:15:56 +1100

    build: setup project

---
+++ src/utils/parseInput.js
@@ -0,0 +1,10 @@
+export const parseInput = (input) => {
+  // expected input: 1 AUD to USD
+  // TODO: parse or tokenize the input string
+
+  return {
+    fromAmount: '',
+    fromCurrency: '',
+    toCurrency: '',
+  };
+};
